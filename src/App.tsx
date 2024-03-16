2a5d561323e2ffca25eed8cbf576cf3c22c2d422
mohsen-ameli <mohsen.ameli83@gmail.com>
2024-03-15 22:33:58 -0400

    added better drag n drop for different initial positions

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { create } from "zustand"
 
 type BlockType = {
   id: number
-  x: number
-  y: number
   type: "if" | "set"
-  children: Set<number>
+  children: number[]
   springApi?: SpringRef<{ x: number; y: number }>
   ref?: React.MutableRefObject<HTMLDivElement>
+  initialX: number
+  initialY: number
 }
 
 type StoreType = {
@@ -21,43 +21,43 @@ const useBlockStore = create<StoreType>(set => ({
   blocks: [
     {
       id: 0,
-      x: 0,
-      y: 0,
       type: "set",
-      children: new Set(),
+      children: [],
       ref: null!,
+      initialX: 20,
+      initialY: 20,
     },
     {
       id: 1,
-      x: 0,
-      y: 0,
       type: "if",
-      children: new Set(),
+      children: [],
       ref: null!,
+      initialX: 140,
+      initialY: 20,
     },
     {
       id: 2,
-      x: 0,
-      y: 0,
       type: "set",
-      children: new Set(),
+      children: [],
       ref: null!,
+      initialX: 260,
+      initialY: 20,
     },
     {
       id: 3,
-      x: 0,
-      y: 0,
       type: "set",
-      children: new Set(),
+      children: [],
       ref: null!,
+      initialX: 380,
+      initialY: 20,
     },
     {
       id: 4,
-      x: 0,
-      y: 0,
       type: "if",
-      children: new Set(),
+      children: [],
       ref: null!,
+      initialX: 500,
+      initialY: 20,
     },
   ],
 }))
@@ -66,6 +66,10 @@ const OFFSET = 20
 
 function SetBlock({ id }: { id: number }) {
   const ref = useRef<HTMLDivElement>(null!)
+  const blocks = useBlockStore.getState().blocks
+  const initialX = blocks[id].initialX
+  const initialY = blocks[id].initialY
+
   const [{ x, y }, api] = useSpring(() => ({
     x: 0,
     y: 0,
@@ -99,20 +103,21 @@ function SetBlock({ id }: { id: number }) {
               clientY >= rect.top
             ) {
               api.start({
-                x: rect.x + OFFSET,
-                y: rect.y + rect.height * (block.children.size + 1),
+                x: rect.x - initialX + OFFSET,
+                y:
+                  rect.y - initialY + rect.height * (block.children.length + 1),
               })
               useBlockStore.setState(prev => {
-                prev.blocks[block.id].children.add(id)
+                // if the block is not already in the children array
+                if (!prev.blocks[block.id].children.includes(id))
+                  prev.blocks[block.id].children.push(id)
                 return prev
               })
-            } else if (block.children.has(id)) {
-              console.log("here")
+            } else if (block.children.includes(id)) {
               useBlockStore.setState(prev => {
-                const childrenArray = Array.from(prev.blocks[block.id].children)
-                const index = childrenArray.indexOf(id)
-                childrenArray.splice(index, 1)
-                prev.blocks[block.id].children = new Set(childrenArray)
+                prev.blocks[block.id].children = prev.blocks[
+                  block.id
+                ].children.filter(child => child !== id)
                 return prev
               })
             }
@@ -134,7 +139,7 @@ function SetBlock({ id }: { id: number }) {
     <animated.div
       className="border absolute bg-cyan-300 w-fit h-fit p-4"
       {...bind()}
-      style={{ x, y }}
+      style={{ x, y, top: initialY, left: initialX }}
       ref={ref}
     >
       Set Block
@@ -145,8 +150,10 @@ function SetBlock({ id }: { id: number }) {
 function IfBlock({ id }: { id: number }) {
   const ref = useRef<HTMLDivElement>(null!)
   const blocks = useBlockStore(state => state.blocks)
+  const initialX = blocks[id].initialX
+  const initialY = blocks[id].initialY
 
-  const [{ x, y }, api2] = useSpring(() => ({
+  const [{ x, y }, api] = useSpring(() => ({
     x: 0,
     y: 0,
   }))
@@ -154,29 +161,67 @@ function IfBlock({ id }: { id: number }) {
   useEffect(() => {
     useBlockStore.setState(prev => {
       prev.blocks[id].ref = ref
-      prev.blocks[id].springApi = api2
+      prev.blocks[id].springApi = api
       return prev
     })
   }, [])
 
   const bind = useGesture(
     {
+      onDragEnd: info => {
+        // @ts-ignore
+        const { clientX, clientY } = info.event
+        const blocks = useBlockStore.getState().blocks
+
+        for (const block of blocks) {
+          if (block.type === "if" && block.id !== id && block.ref) {
+            const rect = block.ref.current.getBoundingClientRect()
+            // if we are appending this block to another block
+            if (
+              clientX <= rect.right &&
+              clientX >= rect.left &&
+              clientY <= rect.bottom &&
+              clientY >= rect.top
+            ) {
+              // api.start({
+              //   x: rect.x - OFFSET,
+              //   y: 0, //+ rect.height * (block.children.length + 1),
+              // })
+              useBlockStore.setState(prev => {
+                // if the block is not already in the children array
+                if (!prev.blocks[block.id].children.includes(id))
+                  prev.blocks[block.id].children.push(id)
+                return prev
+              })
+            } else if (block.children.includes(id)) {
+              useBlockStore.setState(prev => {
+                prev.blocks[block.id].children = prev.blocks[
+                  block.id
+                ].children.filter(child => child !== id)
+                return prev
+              })
+            }
+          }
+        }
+      },
       onDrag: ({ down, offset: [x, y] }) => {
         if (!down) return
-        const Y_VALUE_TO_REMEMBER = 137 //y.get()
         const rect = ref.current.getBoundingClientRect()
 
-        api2.start({ x, y })
+        api.start({ x, y })
 
         // could make this into a function to tidy things up,
         // and run it every time a drag ends.
-        if (blocks[id].children.size > 0) {
+        if (blocks[id].children.length > 0) {
           for (const child of blocks[id].children) {
-            const childrenArray = Array.from(blocks[id].children)
-            const index = childrenArray.indexOf(child)
+            // print the current ref.current's y value
             blocks[child].springApi?.start({
-              x: x + rect.width + OFFSET,
-              y: y + Y_VALUE_TO_REMEMBER + rect.height * index,
+              x: x + initialX - blocks[child].initialX + OFFSET,
+              y:
+                y +
+                initialY -
+                blocks[child].initialY +
+                rect.height * (blocks[id].children.indexOf(child) + 1),
             })
           }
         }
@@ -191,9 +236,9 @@ function IfBlock({ id }: { id: number }) {
 
   return (
     <animated.div
-      className="border absolute left-20 top-20 bg-red-300 w-fit h-fit p-4"
+      className="border absolute bg-red-300 w-fit h-fit p-4"
       {...bind()}
-      style={{ x, y }}
+      style={{ x, y, top: initialY, left: initialX }}
       ref={ref}
     >
       If Block
