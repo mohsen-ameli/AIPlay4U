6b125b61226e1c94c245cfb8e7ba6e6b2b1e727d
NlSEMONO <fmadarang2542@hotmail.com>
2024-03-22 23:46:36 -0400

    cleaned up updateChildrenDepth

--- src/utils/UpdateChildrenDepth.ts
+++ src/utils/UpdateChildrenDepth.ts
@@ -9,14 +9,6 @@ export default function updateChildrenDepth(
   depth: number = 0,
 ) {
   if (!block) return -1
-  console.log(
-    "call ",
-    block.id,
-    "blockDepth: ",
-    block.blockDepth,
-    "end id",
-    endBlockId,
-  )
 
   if (block.id === endBlockId) {
     if (block) block.blockDepth = currDepth + depth
@@ -24,36 +16,13 @@ export default function updateChildrenDepth(
   }
 
   block.blockDepth = currDepth + depth + 1
-  // console.log("new depth: ", block.blockDepth)
-  // let curr: Block | null = block
   if (block.type === "if") {
     const newEndId = block.id + 1
-    while (block) {
-      // console.log("caller if", block.id)
-      const a = updateChildrenDepth(blocks, block.next, currDepth, newEndId, depth + 1) //prettier-ignore
-      if (block.id === newEndId) {
-        break
-      }
-      block = block.next
-      if (a > 0) {
-        console.log("hi")
-        block = blocks[a]
-        break
-      }
+    const a = updateChildrenDepth(blocks, block.next, currDepth, newEndId, depth + 1) //prettier-ignore
+    if (a > 0) {
+      block = blocks[a]
     }
-    // if (curr) {
-    //   console.log("caller after while", curr.id)
-    //   return updateChildrenDepth(
-    //     blocks,
-    //     curr.next,
-    //     currDepth,
-    //     endBlockId,
-    //     depth,
-    //   )
-    // }
-    // return -1
   }
   if (!block) return -1
-  // console.log("caller state", block.id)
   return updateChildrenDepth(blocks, block.next, currDepth, endBlockId, depth)
 }
